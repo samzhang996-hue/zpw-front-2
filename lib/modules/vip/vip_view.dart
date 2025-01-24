f962b7dd267043a8b896ef854e84d2c48fe9ed8e
欧阳 <7666566+ouyangmmnn@user.noreply.gitee.com>
2025-01-24 17:12:07 +0800

    fix:dj

--- lib/modules/vip/vip_view.dart
+++ lib/modules/vip/vip_view.dart
@@ -20,8 +20,7 @@ class VipPage extends BaseStatefulWidget {
   BaseWidgetState<VipPage> getState() => _VipPageState();
 }
 
-class _VipPageState extends BaseWidgetState<VipPage>
-    with WidgetsBindingObserver {
+class _VipPageState extends BaseWidgetState<VipPage> with WidgetsBindingObserver {
   final logic = Get.find<VipLogic>();
   final state = Get.find<VipLogic>().state;
   late VideoPlayerController _controller;
@@ -87,12 +86,8 @@ class _VipPageState extends BaseWidgetState<VipPage>
           rk8 = state.vipBean.vipList?[state.itemIndex].remark8;
           rk10 = state.vipBean.vipList?[state.itemIndex].remark10;
           // logic.onSatePay(state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.defaultPayKeyType ?? 0);
-          state.isWx =
-              state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.isWxPay ??
-                  0;
-          state.isZfb = state
-                  .vipBean.vipList?[state.itemIndex].vipPriceOutput?.isZfbPay ??
-              0;
+          state.isWx = state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.isWxPay ?? 0;
+          state.isZfb = state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.isZfbPay ?? 0;
           Log.d("pay---1---${state.statePay.value}");
         } else {
           rk9 = "";
@@ -159,16 +154,14 @@ class _VipPageState extends BaseWidgetState<VipPage>
                             child: Container(
                               margin: EdgeInsets.only(top: 10.w, bottom: 10.w),
                               child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
                                 children: [
                                   Visibility(
                                       visible: state.isZfb == 1,
                                       child: InkWell(
                                         onTap: () {
                                           logic.onSatePay(0);
-                                          Log.d(
-                                              "pay---${state.statePay.value}");
+                                          Log.d("pay---${state.statePay.value}");
                                         },
                                         child: Container(
                                           height: 47.w,
@@ -192,9 +185,7 @@ class _VipPageState extends BaseWidgetState<VipPage>
                                               ),
                                               Obx(() {
                                                 return Image.asset(
-                                                  state.statePay.value != 1
-                                                      ? "checked.png".vip
-                                                      : "un_check.png".vip,
+                                                  state.statePay.value != 1 ? "checked.png".vip : "un_check.png".vip,
                                                   width: 14.w,
                                                 );
                                               }),
@@ -230,9 +221,7 @@ class _VipPageState extends BaseWidgetState<VipPage>
                                               ),
                                               Obx(() {
                                                 return Image.asset(
-                                                  state.statePay.value == 1
-                                                      ? "checked.png".vip
-                                                      : "un_check.png".vip,
+                                                  state.statePay.value == 1 ? "checked.png".vip : "un_check.png".vip,
                                                   width: 14.w,
                                                 );
                                               }),
@@ -246,8 +235,7 @@ class _VipPageState extends BaseWidgetState<VipPage>
                           ),
                           InkWell(
                             onTap: () {
-                              var vp = state.vipBean.vipList?[state.itemIndex]
-                                  .vipPriceOutput;
+                              var vp = state.vipBean.vipList?[state.itemIndex].vipPriceOutput;
                               state.goodsId = vp?.id ?? 0;
                               if (state.statePay.value == 1) {
                                 state.payKeyType = 1;
@@ -263,25 +251,14 @@ class _VipPageState extends BaseWidgetState<VipPage>
                                   return;
                                 }
                               }
-                              state.isWx = state
-                                      .vipBean
-                                      .vipList?[state.itemIndex]
-                                      .vipPriceOutput
-                                      ?.isWxPay ??
-                                  0;
-                              state.isZfb = state
-                                      .vipBean
-                                      .vipList?[state.itemIndex]
-                                      .vipPriceOutput
-                                      ?.isZfbPay ??
-                                  0;
+                              state.isWx = state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.isWxPay ?? 0;
+                              state.isZfb = state.vipBean.vipList?[state.itemIndex].vipPriceOutput?.isZfbPay ?? 0;
                               if (!state.isCheck.value) {
                                 CustomSureVipDialogUtils.showCustomDialog(
                                     context: context,
                                     onPressed: () {
                                       if (Platform.isIOS) {
-                                        logic.buyEngin
-                                            .buyProduct(vp?.iosProductId);
+                                        logic.buyEngin.buyProduct(vp?.iosProductId);
                                         return;
                                       }
                                       logic.onSelected(true);
@@ -294,13 +271,10 @@ class _VipPageState extends BaseWidgetState<VipPage>
                             child: Stack(
                               children: [
                                 Container(
-                                  margin: EdgeInsets.only(
-                                      left: 17.w, right: 17.w, top: 17.w),
+                                  margin: EdgeInsets.only(left: 17.w, right: 17.w, top: 17.w),
                                   height: 54.w,
                                   width: double.infinity,
-                                  decoration: BoxDecoration(
-                                      color: const Color(0xffFF2E7E),
-                                      borderRadius: BorderRadius.circular(27)),
+                                  decoration: BoxDecoration(color: const Color(0xffFF2E7E), borderRadius: BorderRadius.circular(27)),
                                   child: Center(
                                       child: CommText(
                                     text: rk8,
@@ -318,11 +292,7 @@ class _VipPageState extends BaseWidgetState<VipPage>
                                         child: Container(
                                           width: 183.w,
                                           height: 27.w,
-                                          decoration: BoxDecoration(
-                                              image: DecorationImage(
-                                                  image: AssetImage(
-                                                      "vip_btn_tip.png".vip),
-                                                  fit: BoxFit.cover)),
+                                          decoration: BoxDecoration(image: DecorationImage(image: AssetImage("vip_btn_tip.png".vip), fit: BoxFit.cover)),
                                           child: CommText(
                                             text: rk10,
                                             fontSize: 13.sp,
@@ -337,17 +307,14 @@ class _VipPageState extends BaseWidgetState<VipPage>
                             ),
                           ),
                           Container(
-                            margin: EdgeInsets.only(
-                                left: 20.w, top: 10.w, bottom: 25.w),
+                            margin: EdgeInsets.only(left: 20.w, top: 10.w, bottom: 25.w),
                             child: Row(
                               children: [
                                 InkWell(
                                   child: Row(
                                     children: [
                                       Image.asset(
-                                        state.isCheck.value
-                                            ? "checked.png".vip
-                                            : "un_check.png".vip,
+                                        state.isCheck.value ? "checked.png".vip : "un_check.png".vip,
                                         width: 14.w,
                                       ),
                                       CommText(
@@ -394,29 +361,44 @@ class _VipPageState extends BaseWidgetState<VipPage>
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        width: 80.w,
-                        height: 30.w,
-                        // color: Colors.red,
-                        padding: EdgeInsets.only(left: 16.w, top: 4),
-                        margin: EdgeInsets.only(top: 40.w, left: 0.w),
-                        child: GestureDetector(
-                          onTap: () {
-                            // Log.e("msg");
-                            if (HandleTool.instance.isMember) {
-                              Get.back();
-                            } else {
-                              CustomExitVipDialogUtils2.showCustomDialog(
-                                  context: context, onPressed: () {});
-                            }
-                          },
+                      InkWell(
+                        child: Container(
+                          width: 80.w,
+                          height: 30.w,
+                          // color: Colors.red,
+                          padding: EdgeInsets.only(left: 16.w, top: 4),
+                          margin: EdgeInsets.only(top: 40.w, left: 0.w),
                           child: Align(
                               alignment: Alignment.topLeft,
                               child: Image.asset(
                                 "back.png".comm,
                                 width: 16.w,
                               )),
+                          // child: GestureDetector(
+                          //   onTap: () {
+                          //     Log.e("msg----");
+                          //     if (HandleTool.instance.isMember) {
+                          //       Get.back();
+                          //     } else {
+                          //       CustomExitVipDialogUtils2.showCustomDialog(
+                          //           context: context, onPressed: () {});
+                          //     }
+                          //   },
+                          //   child: Align(
+                          //       alignment: Alignment.topLeft,
+                          //       child: Image.asset(
+                          //         "back.png".comm,
+                          //         width: 16.w,
+                          //       )),
+                          // ),
                         ),
+                        onTap: () {
+                          if (HandleTool.instance.isMember) {
+                            Get.back();
+                          } else {
+                            CustomExitVipDialogUtils2.showCustomDialog(context: context, onPressed: () {});
+                          }
+                        },
                       ),
                       Visibility(
                         visible: Platform.isIOS,
@@ -482,9 +464,7 @@ class _VipPageState extends BaseWidgetState<VipPage>
                           borderRadius: BorderRadius.circular(12.0),
                           color: Color(0xFF141414),
                           border: Border.all(
-                            color: isSelect
-                                ? Color(0xFFFF2E7E)
-                                : Colors.transparent,
+                            color: isSelect ? Color(0xFFFF2E7E) : Colors.transparent,
                             width: isSelect ? 2.0 : 0.0, // 你可以根据需要调整边框宽度
                           )),
                       child: Column(
@@ -508,18 +488,14 @@ class _VipPageState extends BaseWidgetState<VipPage>
                                   child: CommText(
                                     text: "¥",
                                     fontSize: 13.sp,
-                                    textColor: isSelect
-                                        ? Color(0xFFFF2E7E)
-                                        : Colors.white,
+                                    textColor: isSelect ? Color(0xFFFF2E7E) : Colors.white,
                                     fontWeight: FontWeight.w500,
                                   ),
                                 ),
                                 CommText(
                                   text: vp?.remark3 ?? "0",
                                   fontSize: 27.sp,
-                                  textColor: isSelect
-                                      ? Color(0xFFFF2E7E)
-                                      : Colors.white,
+                                  textColor: isSelect ? Color(0xFFFF2E7E) : Colors.white,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ],
