6663fef7109bcc33db2c416bbd84c8abc3ac19d5
欧阳 <7666566+ouyangmmnn@user.noreply.gitee.com>
2025-02-06 17:54:27 +0800

    add:如果是ios 就剔除恢复功能

--- lib/modules/wf/wf_logic.dart
+++ lib/modules/wf/wf_logic.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
 import 'package:zpw/base/base_getx_controller.dart';
 import 'package:zpw/model/list_photo_group_bean.dart';
@@ -25,7 +27,11 @@ class WfLogic extends BaseGetxController {
         },
         success: (isSuccess, code, message, results) {
           if (isSuccess == true && results.isNotEmpty) {
-            state.listPhotoGroupBean = results;
+            if (Platform.isIOS) {
+              state.listPhotoGroupBean = results.where((item) => item.frontType != 'SJHF').toList();
+            } else {
+              state.listPhotoGroupBean = results;
+            }
             Log.d("list----${state.listPhotoGroupBean[1].toJson()}");
             update();
           }
