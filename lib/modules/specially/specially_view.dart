7845ba672f0b5d53148718e97abd0e0a05ccfea9
zhangdong <123@qq.com>
2025-02-08 14:25:12 +0800

    fix: 滚动背景颜色

--- lib/modules/specially/specially_view.dart
+++ lib/modules/specially/specially_view.dart
@@ -75,9 +75,7 @@ class _SpeciallyPageState extends State<SpeciallyPage>
     _scrollViewController?.addListener(() {
       double? offset = _scrollViewController?.offset;
 
-      print("offset:${offset}");
-
-      if ((offset! > 166.36363636363615) == true) {
+      if ((offset! >= kToolbarHeight) == true) {
         // 滚动到最顶部时，背景色为淡紫色
         setState(() {
           _backgroundColor = Colors.white;
