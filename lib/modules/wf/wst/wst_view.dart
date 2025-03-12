281d19dcba4d4cc2eaea4c66af74ab5335de7bcd
zhangdong <123@qq.com>
2025-03-12 11:17:12 +0800

    fix: 文生图上下滑动

--- lib/modules/wf/wst/wst_view.dart
+++ lib/modules/wf/wst/wst_view.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:zpw/base/base_stateful_widget.dart';
 import 'package:zpw/common/constant.dart';
 import 'package:zpw/common/qds_Image.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:zpw/common/style.dart';
 import 'package:zpw/common/view/comm_text.dart';
 import 'package:zpw/modules/mine/works/works_view.dart';
 import 'package:zpw/modules/vip/vip_view.dart';
 import 'package:zpw/modules/wf/makewst/makewst_view.dart';
 import 'package:zpw/utils/handle_tool.dart';
+
 import 'wst_logic.dart';
 
 class WstPage extends BaseStatefulWidget {
@@ -29,59 +29,77 @@ class WstPageState extends BaseWidgetState {
           height: double.infinity,
           child: Stack(
             children: [
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    YAppBar(
-                        title: "文生图",
-                        right: InkWell(
-                            onTap: () {
-                              gotoPushPage(WorksPage());
-                            },
-                            child: Row(
-                              children: [
-                                Image.asset(
-                                  "my_work_ic.png".make,
-                                  width: 22.w,
-                                  height: 22.w,
-                                ),
-                                CommText(
-                                  text: "作品",
-                                  fontSize: 13.sp,
-                                  textColor: Color(0xff191919),
-                                  fontWeight: FontWeight.bold,
-                                )
-                              ],
-                            ))),
-                    Container(margin: EdgeInsets.all(10.w), child: QdsImageCorner(state.showImgGif.value, double.infinity, 458.w, 16.w, fit: BoxFit.cover)),
-                    Container(
-                      margin: EdgeInsets.all(10.w),
-                      width: double.infinity,
-                      decoration: BoxDecoration(color: Color(0xffF9F9F9), borderRadius: BorderRadius.circular(16.w)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              margin: EdgeInsets.only(left: 13.w, top: 15.w),
-                              child: CommText(
-                                text: "提示词Prom",
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.sp,
-                                textColor: Colors.black,
-                              )),
-                          Container(
-                              margin: EdgeInsets.only(top: 8.w, left: 13.w, right: 13.w, bottom: 16.w),
-                              child: CommText(
-                                text: state.funcValue.value,
-                                fontSize: 14.sp,
-                                textColor: Color(0xff818181),
-                              ))
-                        ],
-                      ),
+              Column(children: [
+                YAppBar(
+                    title: "文生图",
+                    right: InkWell(
+                        onTap: () {
+                          gotoPushPage(WorksPage());
+                        },
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              "my_work_ic.png".make,
+                              width: 22.w,
+                              height: 22.w,
+                            ),
+                            CommText(
+                              text: "作品",
+                              fontSize: 13.sp,
+                              textColor: Color(0xff191919),
+                              fontWeight: FontWeight.bold,
+                            )
+                          ],
+                        ))),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                            margin: EdgeInsets.all(10.w),
+                            child: QdsImageCorner(state.showImgGif.value,
+                                double.infinity, 458.w, 16.w,
+                                fit: BoxFit.cover)),
+                        Container(
+                          margin: EdgeInsets.all(10.w),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0xffF9F9F9),
+                              borderRadius: BorderRadius.circular(16.w)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  margin:
+                                      EdgeInsets.only(left: 13.w, top: 15.w),
+                                  child: CommText(
+                                    text: "提示词Prom",
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.sp,
+                                    textColor: Colors.black,
+                                  )),
+                              Container(
+                                  margin: EdgeInsets.only(
+                                      top: 8.w,
+                                      left: 13.w,
+                                      right: 13.w,
+                                      bottom: 16.w),
+                                  child: CommText(
+                                    text: state.funcValue.value,
+                                    fontSize: 14.sp,
+                                    textColor: Color(0xff818181),
+                                  ))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 80.w,
+                        )
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                )
+              ]),
               Positioned(
                 bottom: 16.w,
                 left: 0,
@@ -89,7 +107,10 @@ class WstPageState extends BaseWidgetState {
                 child: InkWell(
                     onTap: () {
                       if (HandleTool.instance.isMember) {
-                        gotoPushPage(MakewstPage(), arguments: {"funcValue": state.funcValue.value, "funcId": state.funcId.value});
+                        gotoPushPage(MakewstPage(), arguments: {
+                          "funcValue": state.funcValue.value,
+                          "funcId": state.funcId.value
+                        });
                       } else {
                         gotoPushPage(VipPage());
                       }
