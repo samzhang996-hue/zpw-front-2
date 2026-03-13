2328aa3bb2d002a50bc8ffd63170ccc86e2a243b
zzz <123@qq.com>
2026-03-13 17:57:09 +0800

    iOS 运行成功

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
 class Config {
   static const String kWechatAppID = 'wx5bdeda6fcfb891c8';
-  static const String kWechatUniversalLink = 'http://photo.jlhkj168.cn/photoking/';
+  static const String kWechatUniversalLink = 'https://photo.jlhkj168.cn/photoking/';
 }
